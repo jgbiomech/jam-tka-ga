e24154c7dcd77f4f628cdd4c5b28591b12687f8f
jgbiomech <glendayj@hss.edu>
2023-06-14 17:11:38 -0400

    Added WIP BlenderAR code. Working on settle sims

--- jam-settle-tka.py
+++ jam-settle-tka.py
@@ -5,37 +5,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Import the necessary functions from myFuncs.py:
-from myFuncs import configure_knee_flex 
+# from myFuncs import ***
 
 osim.Logger.setLevelString("info")
 useVisualizer=False
 
-knee_type = "TKA"
-subjectID = "DM"
+### Configure inputs:
+sim_acc = 1e-3
 
-base_path        = "C:\\opensim-jam\\jam-tka-ga"
-model_file       = "..\\jam-resources\\models\knee_"+knee_type+"\\grand_challenge\\"+subjectID+"\\"+subjectID+".osim"
-results_basename = "settling"
-inputs_dir       = base_path+"\\inputs"
-results_dir      = base_path+"\\results"
-settling_dir     =  results_dir+"\\settling\\"+knee_type
+ligIDs          = ["Nominal","minOffset","maxOffset"]
+slackOffset     = [1,0.95,1.05]
+print_slacks    = 0
+knee_type       = "TKA"
+subjectID       = "DM"
+base_path       = "C:\\opensim-jam\\jam-tka-ga"
+inputs_dir      = base_path+"\\inputs"
+results_dir     = base_path+"\\results"
+settling_dir    =  results_dir+"\\settling\\"+knee_type
+model_dir       = "..\\jam-resources\\models\knee_"+knee_type+"\\grand_challenge\\"+subjectID
+base_model_file = model_dir+"\\"+subjectID+".osim"
 
-### Create Input Files
-
+### Configure folders:
 os.makedirs(inputs_dir, exist_ok=True)
 os.makedirs(results_dir, exist_ok=True)
 os.makedirs(settling_dir, exist_ok=True)
+os.makedirs(model_dir, exist_ok=True)
 
-comakIK_settings_file  = inputs_dir+"\\comak_IK_settings.xml"
-jnt_mech_settings_file = inputs_dir+"\\joint_mechanics_settings.xml"
-
-### Perform Simulation with COmakIKTool:
+### Configure the generic COMAKInverseKinematicsTool settings:
 IKtool = osim.COMAKInverseKinematicsTool()
-IKtool.set_model_file(model_file)
 IKtool.set_results_directory(settling_dir)
-IKtool.set_results_prefix(results_basename)
 IKtool.set_perform_secondary_constraint_sim(True)
-IKtool.set_secondary_constraint_sim_integrator_accuracy(1e-3)
+IKtool.set_secondary_constraint_sim_integrator_accuracy(sim_acc)
 IKtool.set_secondary_constraint_sim_internal_step_limit(10000)
 IKtool.set_print_secondary_constraint_sim_results(True)
 IKtool.set_secondary_constraint_sim_settle_threshold(1e-4)
@@ -56,7 +56,7 @@ IKtool.set_secondary_coupled_coordinate_stop_value(0)
 IKtool.set_secondary_constraint_sim_sweep_time(0)
 IKtool.set_perform_inverse_kinematics(False)
 IKtool.set_use_visualizer(useVisualizer)
-# Placeholder:
+# Placeholder settings:
 IKtool.set_secondary_constraint_function_file('Unassigned')
 IKtool.set_constraint_function_num_interpolation_points
 IKtool.set_coordinate_file('Unassigned')
@@ -69,26 +69,9 @@ IKtool.set_time_range(1,0)
 IKtool.set_report_errors(False)
 IKtool.set_report_marker_locations(False)
 IKtool.set_constrained_model_file('Unassigned')
-IKtool.printToXML(comakIK_settings_file)
-print('Running COMAK-IK Tool...')
-IKtool.run()
 
-### Clean up COMAK IK files:
-files = os.listdir(settling_dir)
-
-for file in files:
-    if 'sweep' in file:
-        os.remove(settling_dir+'\\'+file)
-
-for file in files:
-    if 'settle' in file:
-        os.rename(settling_dir+'\\'+file,settling_dir+'\\'+'settling_states.sto')
-        
-### Perform Analysis with JointMechanicsTool
+### Configure the generic JointMechanicsTool settings:
 jnt_mech = osim.JointMechanicsTool()
-jnt_mech.set_model_file(model_file)
-jnt_mech.set_input_states_file(settling_dir + '\\' + results_basename + '_states.sto')
-jnt_mech.set_results_file_basename(results_basename)
 jnt_mech.set_results_directory(settling_dir)
 jnt_mech.set_start_time(-1)
 jnt_mech.set_stop_time(-1)
@@ -107,7 +90,69 @@ jnt_mech.set_write_h5_file(True)
 jnt_mech.set_h5_kinematics_data(True)
 jnt_mech.set_h5_states_data(True)
 jnt_mech.set_use_visualizer(useVisualizer)
-jnt_mech.printToXML(jnt_mech_settings_file)
 
-print('Running JointMechanicsTool...')
-jnt_mech.run()
+idx = 0
+for ligID in ligIDs:
+
+    results_basename = "settling_"+ligID # Simulation-specific filename
+
+    # Settings file names:
+    comakIK_settings_file  = inputs_dir+"\\comak_IK_settings_"+results_basename+".xml"
+    jnt_mech_settings_file = inputs_dir+"\\joint_mechanics_settings_"+results_basename+".xml"
+
+    # Modify model:
+    myModel = osim.Model(base_model_file)
+
+    ForceSet = myModel.getForceSet()
+    ForceSetSize = ForceSet.getSize()
+
+    for ii in range(0,ForceSetSize-1):
+        f = ForceSet.get(ii)
+        if f.getConcreteClassName() == 'Blankevoort1991Ligament':
+            slack_length = float(f.getPropertyByName('slack_length').toString())
+            osim.PropertyHelper.setValueDouble(slackOffset[idx]*slack_length,f.getPropertyByName('slack_length'))
+            if print_slacks == 1:
+                print(f.getName()," - default slack = ",np.round(slack_length*1000,1)," mm - updated slack = ",np.round(slackOffset[idx]*slack_length*1000,1))
+            
+
+    # Print modified model:
+    model_file = model_dir+"\\"+subjectID+"_"+ligID+".osim"
+    myModel.printToXML(model_dir+"\\"+subjectID+"_"+ligID+".osim")
+    
+    #############################################################################
+    ### Settling simulation - using the COMAK-IK Tool:
+    #############################################################################
+
+    # Simulation-specific inputs: 
+    IKtool.set_model_file(model_file)
+    IKtool.set_results_prefix(results_basename)
+    IKtool.printToXML(comakIK_settings_file) # Print simulation-specific file
+
+    # Run the IK tool to determine the settled knee position
+    print('Running COMAK-IK Tool...')
+    IKtool.run()
+
+    # Clean up COMAK IK files:
+    files = os.listdir(settling_dir)
+
+    for file in files:
+        if 'sweep' in file:
+            os.remove(settling_dir+'\\'+file)
+
+    for file in files:
+        if 'settle' in file:
+            os.rename(settling_dir+'\\'+file,settling_dir+'\\'+results_basename+'_states.sto')
+
+    #############################################################################
+    ### Perform analysis using the Joint Mechanics Tool:
+    #############################################################################       
+
+    jnt_mech.set_model_file(model_file)
+    jnt_mech.set_input_states_file(settling_dir + '\\' + results_basename + '_states.sto')
+    jnt_mech.set_results_file_basename(results_basename)
+    jnt_mech.printToXML(jnt_mech_settings_file)
+
+    print('Running JointMechanicsTool...')
+    jnt_mech.run()
+
+    idx += 1
